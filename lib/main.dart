df6c5ae3811082ef928fcbcbd42a2bb31d267501
Tyler <wsc03002@example.com>
2023-01-30 01:49:48 +0900

    update:: Styled

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,35 @@ import 'package:u_and_i/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: HomeScreen(),
+    MaterialApp(
+      theme: ThemeData(
+          fontFamily: 'sunflower',
+          textTheme: const TextTheme(
+            // ignore: deprecated_member_use
+            headline1: TextStyle(
+              color: Colors.white,
+              fontSize: 80.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'parisienne',
+            ),
+            // ignore: deprecated_member_use
+            headline2: TextStyle(
+              color: Colors.white,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700,
+            ),
+            // ignore: deprecated_member_use
+            bodyText1: TextStyle(
+              color: Colors.white,
+              fontSize: 30.0,
+            ),
+            // ignore: deprecated_member_use
+            bodyText2: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
+            ),
+          )),
+      home: const HomeScreen(),
     ),
   );
 }
