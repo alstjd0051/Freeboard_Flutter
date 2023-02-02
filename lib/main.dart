c14e017b122804d00d01e1ff27b46cdee2c8f816
Tyler <wsc03002@example.com>
2023-02-02 15:59:09 +0900

    sizedBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Text(
-          'Tyler',
-          style: TextStyle(
-              fontSize: 16.0, fontWeight: FontWeight.w700, color: Colors.blue),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Appbar')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Icon(Icons.star), Icon(Icons.star), Icon(Icons.star)],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ],
+            ),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
