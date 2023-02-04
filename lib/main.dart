c1726e3ca41f00db6bba19a0f4a77e7b4fdcc2bc
Tyler <wsc03002@example.com>
2023-02-04 23:56:43 +0900

    Add:: random List

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    // final wordPair = WordPair.random();
     return MaterialApp(
+      title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(title: Text('Appbar')),
-        body: Container(
-          width: 50,
-          height: 50,
-          color: Colors.blue,
-          // margin: EdgeInsets.all(20),
-          padding: EdgeInsets.fromLTRB(20, 10, 50, 30),
+        // appBar: AppBar(
+        //   title: Text('Flutter Demo'),
+        // ),
+        body: Center(
+          // child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
       ),
     );
   }
