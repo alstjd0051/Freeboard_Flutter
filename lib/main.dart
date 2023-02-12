4d72f8300976371f846d136b218bb6e8584a69a9
tyler <wsc03002@naver.com>
2023-02-12 21:35:37 +0900

    update:: Logo

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,37 @@ import 'package:flutter/material.dart';
 // void main() {
 //   runApp(SplashScreen());
 // }
-void main() => runApp(SplashScreen());
+void main() => runApp(const SplashScreen());
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            color: Colors.orange,
+            color: Color(0x0FA4A19E),
           ),
-          child: Center(
-            child: Image.asset(
-              'assets/Logo.jpg',
-            ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/Logo.jpg',
+                    width: 200,
+                  ),
+                  const CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation(
+                      Colors.white,
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
