166a06e8945e9461f2e3649aa6841997aa628500
tyler <wsc03002@naver.com>
2023-02-12 22:20:58 +0900

    update:: settingsApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:freeboard/screen/home_screen.dart';
 
-// void main() {
-//   runApp(SplashScreen());
-// }
-void main() => runApp(const SplashScreen());
-
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            color: Color(0x0FA4A19E),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/Logo.jpg',
-                    width: 200,
-                  ),
-                  const CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation(
-                      Colors.white,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+// void main() => runApp(
+//       const MaterialApp(
+//         home: HomeScreen(),
+//       ),
+//     );
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  WebViewController? controller;
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Tyler'),
+        centerTitle: true,
+      ),
+      body: WebView(
+          onWebViewCreated: (WebViewController controller) {
+            this.controller = controller;
+          },
+          initialUrl: 'https://blog.codefactory.ai',
+          javascriptMode: JavascriptMode.disabled),
+    );
+  }
+}
